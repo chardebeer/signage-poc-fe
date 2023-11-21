34c278b4ea62ffb08bbf0389b1b4eb1a0629a872
Charlize de Beer <charlizedb@aie.ac>
2023-11-21 14:20:10 +0200

    Delete videos

--- src/components/EventList/index.js
+++ src/components/EventList/index.js
@@ -8,7 +8,8 @@ const EventsContainer = styled.div`
   padding: 20px;
   border-radius: 10px;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-width:calc(100%-10px);`;
+  width:calc(100%-10px);
+`;
 
 const EventItem = styled.div`
   padding: 10px;
